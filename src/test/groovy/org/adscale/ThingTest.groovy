3d5298be631a7dfd04b22ca7b14b3c0a432b7b77
Andrew Oxenburgh <andrew.oxenburgh@adscale.co.nz>
2014-07-26 09:29:16 +1200

    initial

---
+++ src/test/groovy/org/adscale/ThingTest.groovy
@@ -0,0 +1,10 @@
+import org.junit.Test
+
+class ThingTest {
+
+    @Test
+    public void test() throws Exception {
+        println 'khdsfkjhkjfhkdsjh'
+        assert true
+    }
+}
---
+++ src/test/java/org/adscale/AppTest.java
@@ -0,0 +1,13 @@
+package org.adscale;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest{
+
+    @Test
+    public void test() throws Exception {
+        assertTrue(true);
+    }
+}
